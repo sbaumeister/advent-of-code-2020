4a4f390d86d67c60334c4993fc7b1949bd5a9f9d
Stephan Baumeister <stephan@baumeister.name>
2020-12-06 00:24:35 +0100

    Day 1 solution

--- day-01-report-repair/index.ts
+++ day-01-report-repair/index.ts
@@ -1,6 +1,29 @@
-import {readFile} from 'fs'
-import path from "path";
+import {readFileSync} from 'fs'
+import path from "path"
 
-readFile(path.resolve(__dirname, 'input.txt'), (err, data) => {
-    console.log(data);
-});
+const numbers : number[] = readFileSync(path.resolve(__dirname, 'input.txt')).toString()
+    .split("\n")
+    .filter(s => s.length > 0)
+    .map(n => parseInt(n))
+
+type Result = { number1: number, number2: number };
+
+function findNumbers(): Result {
+    for (let k1 in numbers) {
+        let n1 = numbers[k1];
+        for (let k2 in numbers) {
+            if (k1 === k2) {
+                continue
+            }
+            let n2 = numbers[k2];
+            if (n1 + n2 === 2020) {
+                return { number1: n1, number2: n2}
+            }
+        }
+    }
+    throw new Error("Found no number pair that sum to 2020")
+}
+
+const { number1, number2 } = findNumbers();
+console.log(`${number1} * ${number2} = ${number1 * number2}`)
+
