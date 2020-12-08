586cfe542b2e5748ad4a6afd2b0541366c86d835
Stephan Baumeister <stephan@baumeister.name>
2020-12-08 22:12:53 +0100

    Solution for day 3 part 2

--- day-03-toboggan-trajectory/index.ts
+++ day-03-toboggan-trajectory/index.ts
@@ -4,31 +4,40 @@ import path from "path"
 const rows: string[] = readFileSync(path.resolve(__dirname, 'input.txt')).toString()
     .split("\n")
 
-let bottomReached = false;
-let x = 0;
-let y = 0;
-let countTrees = 0;
+function traverseMap(right: number, down: number): number {
+    let bottomReached = false;
+    let x = 0;
+    let y = 0;
+    let countTrees = 0;
 
-do {
-    x += 3;
-    y += 1;
+    do {
+        x += right;
+        y += down;
 
-    if (rows[y].length > 0) {
+        if (rows[y] && rows[y].length > 0) {
 
-        if (rows[y].charAt(x) === '') {
-            x = x - rows[y].length;
-        }
-        if (rows[y].charAt(x) === '#') {
-            // console.log(`# Tree at y = ${y}, x = ${x}`)
-            countTrees++;
+            if (rows[y].charAt(x) === '') {
+                x = x - rows[y].length;
+            }
+            if (rows[y].charAt(x) === '#') {
+                // console.log(`# Tree at y = ${y}, x = ${x}`)
+                countTrees++;
+            } else {
+                // console.log(`. NO tree at y = ${y}, x = ${x}`)
+            }
+
         } else {
-            // console.log(`. NO tree at y = ${y}, x = ${x}`)
+            bottomReached = true;
         }
 
-    } else {
-        bottomReached = true;
-    }
+    } while (!bottomReached)
 
-} while (!bottomReached)
+    return countTrees;
+}
 
-console.log(`Part 1: count trees = ${countTrees}`)
+const countTreesPart1 = traverseMap(3, 1);
+console.log(`Part 1: count trees = ${countTreesPart1}`)
+
+const resultPart2 = countTreesPart1 * traverseMap(1, 1) * traverseMap(5, 1)
+    * traverseMap(7, 1) * traverseMap(1, 2)
+console.log(`Part 2: result = ${resultPart2}`)
