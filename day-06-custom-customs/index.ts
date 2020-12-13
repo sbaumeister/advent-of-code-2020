9c6271ef21b270f479c0a2940f10bfcfa3e5e387
Stephan Baumeister <stephan@baumeister.name>
2020-12-13 18:05:39 +0100

    Day 6 part 2

--- day-06-custom-customs/index.ts
+++ day-06-custom-customs/index.ts
@@ -16,5 +16,25 @@ function calcSumPart1() {
     return sum;
 }
 
-let sum = calcSumPart1();
-console.log(`Part 1: yes answer sum = ${sum}`)
+function calcSumPart2() {
+    let sum = 0
+    for (const group of groups) {
+        const map = new Map<string, number>()
+        const users = group.split("\n")
+        users.flatMap(u => Array.from(u))
+            .forEach(y => map.set(y, (map.get(y) || 0) + 1))
+        for (const el of map) {
+            if (el[1] == users.length) {
+                sum++
+            }
+        }
+        // console.log(`sum: ${sum}`)
+    }
+    return sum;
+}
+
+let sumPart1 = calcSumPart1();
+console.log(`Part 1: sum any yes answers = ${sumPart1}`)
+
+let sumPart2 = calcSumPart2();
+console.log(`Part 2: sum common yes answers = ${sumPart2}`)
