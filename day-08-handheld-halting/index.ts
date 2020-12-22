2ed3acd5f08aef70b5d13691838d596c5cb16696
Stephan Baumeister <stephan@baumeister.name>
2020-12-22 23:14:39 +0100

    WIP: day 8 part 2

--- day-08-handheld-halting/index.ts
+++ day-08-handheld-halting/index.ts
@@ -4,7 +4,6 @@ import path from "path"
 interface Instruction {
     type: string
     value: number
-    visited: boolean
 }
 
 const instructions = new Map<number, Instruction>()
@@ -12,29 +11,73 @@ readFileSync(path.resolve(__dirname, 'input.txt')).toString()
     .split('\n')
     .filter(s => s.length > 0)
     .forEach((s, i) => {
-        instructions.set(i, {type: s.substr(0, 3), value: parseInt(s.substr(4)), visited: false})
+        instructions.set(i, {type: s.substr(0, 3), value: parseInt(s.substr(4))})
     })
 
-let accumulator = 0
-let instructionPointer = 0
-while (true) {
-    const instruction = instructions.get(instructionPointer)
-    if (!instruction) {
-        throw new Error(`Instruction ${instructionPointer} not found`)
+function executeProgram(): boolean {
+    let accumulator = 0
+    let instructionPointer = 0
+    const visitations = new Map<number,boolean>()
+    let programTerminated = false
+    console.log('--- START PROGRAM ---')
+    while (true) {
+        // console.log(instructionPointer)
+        if (instructionPointer == instructions.size) {
+            console.log(`Program terminated!`)
+            console.log(`Part 2: accumulator = ${accumulator}`)
+            programTerminated = true
+            break;
+        }
+        const instruction = instructions.get(instructionPointer)
+        if (!instruction) {
+            throw new Error(`Instruction ${instructionPointer} not found`)
+        }
+        if (visitations.has(instructionPointer)) {
+            console.log(`Loop: instruction ${instructionPointer} about to be visited twice`)
+            console.log(`Part 1: accumulator = ${accumulator}`)
+            programTerminated = false
+            break;
+        }
+        visitations.set(instructionPointer, true)
+        if (instruction.type == 'acc') {
+            accumulator += instruction.value || 0
+            instructionPointer++
+        }
+        if (instruction.type == 'jmp') {
+            instructionPointer += instruction.value || 0
+        }
+        if (instruction.type == 'nop') {
+            instructionPointer++
+        }
     }
-    if (instruction.visited) {
-        console.log(`Part 1: accumulator = ${accumulator}`)
-        break;
-    }
-    if (instruction.type == 'acc') {
-        accumulator += instruction.value || 0
-        instructionPointer++
+    console.log('--- END PROGRAM ---')
+    return programTerminated
+}
+
+function switchInstructionType(instruction: Instruction) {
+    if (instruction.type == 'nop') {
+        instruction.type = 'jmp'
     }
     if (instruction.type == 'jmp') {
-        instructionPointer += instruction.value || 0
+        instruction.type = 'nop'
     }
-    if (instruction.type == 'nop') {
-        instructionPointer++
-    }
-    instruction.visited = true
 }
+
+function part2() {
+    for (const [i, instruction] of instructions) {
+        if (['nop', 'jmp'].indexOf(instruction.type) != -1) {
+            console.log(`Switch instruction ${i}: ${JSON.stringify(instruction)}`)
+            switchInstructionType(instruction)
+            if (executeProgram()) {
+                break;
+            }
+            switchInstructionType(instruction)
+        }
+    }
+}
+
+// Part 1
+executeProgram();
+
+// Part2
+part2();
